8cf08e3effda3e116ef033fd03394a8a3b471bcc
Martin Kamau <martinkimu09@gmail.com>
2022-09-08 08:51:11 +0300

    Corrected added listener

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-//fetch api
-function fetchPlaces() {  
-    fetch("https://zoo-animal-api.herokuapp.com/")
-      .then((resp) => resp.json())
-      .then((json) => getPlaces(json));
-  }
-  
-  function getPlaces(places) {
-    const main = document.querySelector('main');
-    places.forEach(places => {
-      const div = document.createElement('div');
-      div.innerHTML = places.name;
-      main.appendChild(div)[10];
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    getPlaces();
-  });
-
-//added event listener
+//addEventListener
 document.addEventListener('DOMContentLoaded', () => {
-    let btn = document.getElementById('btn');
-    btn.addEventListener('click', () => alert('Booking Successful!'));
-});
+    let btn = document.getElementById('btn')
+    btn.addEventListener ('click', () => alert('Congratulations! Booking was Successful.'))
+})
 
+//
