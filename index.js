4c935eceeacc2ee86ab68288044a23f72e8b7f8b
Martin Kamau <martinkimu09@gmail.com>
2022-09-08 12:58:01 +0300

    Removed Destination button

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     let btn = document.getElementById('btn')
     btn.addEventListener ('click', () => alert('Congratulations! Booking was Successful.'))
+
+
     //submit comment button
     let button = document.getElementById('comment')
     comment.addEventListener('submit', e => {
@@ -11,3 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         document.reset()
     })
 })
+
+//remove the destination button
+document.getElementById('places').remove('button');
