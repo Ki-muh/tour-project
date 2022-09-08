44148d60ff7deae97c66a84706664783be87cbdf
Martin Kamau <martinkimu09@gmail.com>
2022-09-08 07:23:00 +0300

    fetching api

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-//add event listener
-document.querySelector('#btn').addEventListener('click',
-function() {
-    return alert('Booking Successful!')
-}) 
+//fetch api
+function fetchPlaces() {  
+    fetch("https://zoo-animal-api.herokuapp.com/")
+      .then((resp) => resp.json())
+      .then((json) => getPlaces(json));
+  }
+  
+  function getPlaces(places) {
+    const main = document.querySelector('main');
+    places.forEach(places => {
+      const div = document.createElement('div');
+      div.innerHTML = places.name;
+      main.appendChild(div)[10];
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    getPlaces();
+  });
+
+//added event listener
+document.addEventListener('DOMContentLoaded', () => {
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', () => alert('Booking Successful!'));
+});
+
