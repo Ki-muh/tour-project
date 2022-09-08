90cc12a737d403ee8f840966fd7384fb59ecf685
Martin Kamau <martinkimu09@gmail.com>
2022-09-08 10:36:27 +0300

    Added feedback box

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     let btn = document.getElementById('btn')
     btn.addEventListener ('click', () => alert('Congratulations! Booking was Successful.'))
+    //submit comment button
+    let button = document.getElementById('comment')
+    comment.addEventListener('submit', e => {
+        e.preventDefault()
+        let addComment = document.getElementById('comment').value
+        console.log(addComment)
+        document.reset()
+    })
 })
-
-//
